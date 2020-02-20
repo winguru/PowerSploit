e7c219e8869bdfc49b9a9cbdcc9a7e8c08a76151
exploide <me@exploide.net>
2020-02-20 17:53:51 +0100

    Get-RegistryAutoLogon: print results when passwords are found, not only when AutoAdminLogon is enabled

--- Privesc/PowerUp.ps1
+++ Privesc/PowerUp.ps1
@@ -3526,26 +3526,23 @@ https://github.com/rapid7/metasploit-framework/blob/master/modules/post/windows/
     $AutoAdminLogon = $(Get-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoAdminLogon -ErrorAction SilentlyContinue)
     Write-Verbose "AutoAdminLogon key: $($AutoAdminLogon.AutoAdminLogon)"
 
-    if ($AutoAdminLogon -and ($AutoAdminLogon.AutoAdminLogon -ne 0)) {
+    $DefaultDomainName = $(Get-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultDomainName -ErrorAction SilentlyContinue).DefaultDomainName
+    $DefaultUserName = $(Get-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultUserName -ErrorAction SilentlyContinue).DefaultUserName
+    $DefaultPassword = $(Get-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultPassword -ErrorAction SilentlyContinue).DefaultPassword
+    $AltDefaultDomainName = $(Get-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AltDefaultDomainName -ErrorAction SilentlyContinue).AltDefaultDomainName
+    $AltDefaultUserName = $(Get-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AltDefaultUserName -ErrorAction SilentlyContinue).AltDefaultUserName
+    $AltDefaultPassword = $(Get-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AltDefaultPassword -ErrorAction SilentlyContinue).AltDefaultPassword
 
-        $DefaultDomainName = $(Get-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultDomainName -ErrorAction SilentlyContinue).DefaultDomainName
-        $DefaultUserName = $(Get-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultUserName -ErrorAction SilentlyContinue).DefaultUserName
-        $DefaultPassword = $(Get-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultPassword -ErrorAction SilentlyContinue).DefaultPassword
-        $AltDefaultDomainName = $(Get-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AltDefaultDomainName -ErrorAction SilentlyContinue).AltDefaultDomainName
-        $AltDefaultUserName = $(Get-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AltDefaultUserName -ErrorAction SilentlyContinue).AltDefaultUserName
-        $AltDefaultPassword = $(Get-ItemProperty -Path "HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AltDefaultPassword -ErrorAction SilentlyContinue).AltDefaultPassword
-
-        if ($DefaultUserName -or $AltDefaultUserName) {
-            $Out = New-Object PSObject
-            $Out | Add-Member Noteproperty 'DefaultDomainName' $DefaultDomainName
-            $Out | Add-Member Noteproperty 'DefaultUserName' $DefaultUserName
-            $Out | Add-Member Noteproperty 'DefaultPassword' $DefaultPassword
-            $Out | Add-Member Noteproperty 'AltDefaultDomainName' $AltDefaultDomainName
-            $Out | Add-Member Noteproperty 'AltDefaultUserName' $AltDefaultUserName
-            $Out | Add-Member Noteproperty 'AltDefaultPassword' $AltDefaultPassword
-            $Out.PSObject.TypeNames.Insert(0, 'PowerUp.RegistryAutoLogon')
-            $Out
-        }
+    if ($DefaultUserName -or $AltDefaultUserName) {
+        $Out = New-Object PSObject
+        $Out | Add-Member Noteproperty 'DefaultDomainName' $DefaultDomainName
+        $Out | Add-Member Noteproperty 'DefaultUserName' $DefaultUserName
+        $Out | Add-Member Noteproperty 'DefaultPassword' $DefaultPassword
+        $Out | Add-Member Noteproperty 'AltDefaultDomainName' $AltDefaultDomainName
+        $Out | Add-Member Noteproperty 'AltDefaultUserName' $AltDefaultUserName
+        $Out | Add-Member Noteproperty 'AltDefaultPassword' $AltDefaultPassword
+        $Out.PSObject.TypeNames.Insert(0, 'PowerUp.RegistryAutoLogon')
+        $Out
     }
 }
 
